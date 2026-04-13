0ba1bdec62837b3e88018a6d9174af742519046a
alijahsamaroo <alijahsamaroo@gmail.com>
2026-04-13 11:07:05 -0400

    updated input field , fixed  leaderboard html
    input needed to click to a new one each time , fixed it

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,22 +1,39 @@
+function setupDigitInputs() {
+    const inputs = Array.from(document.querySelectorAll(".digit-input"));
 
-async function getUserData(){
-    const response = await fetch('/api/users');
-    return response.json();
+    // if not on the game page, stop
+    if (inputs.length === 0) return;
+
+    inputs.forEach((input, index) => {
+        input.addEventListener("input", function () {
+            // only keep 1 number
+            this.value = this.value.replace(/\D/g, "").slice(0, 1);
+
+            // move to next box automatically
+            if (this.value.length === 1 && index < inputs.length - 1) {
+                inputs[index + 1].focus();
+                inputs[index + 1].select();
+            }
+        });
+
+        input.addEventListener("keydown", function (event) {
+            // go back if backspace on empty box
+            if (event.key === "Backspace" && this.value === "" && index > 0) {
+                inputs[index - 1].focus();
+            }
+
+            // allow left/right arrow movement
+            if (event.key === "ArrowLeft" && index > 0) {
+                inputs[index - 1].focus();
+            }
+
+            if (event.key === "ArrowRight" && index < inputs.length - 1) {
+                inputs[index + 1].focus();
+            }
+        });
+    });
 }
 
-function loadTable(users){
-    const table = document.querySelector('#result');
-    for(let user of users){
-        table.innerHTML += `<tr>
-            <td>${user.id}</td>
-            <td>${user.username}</td>
-        </tr>`;
-    }
-}
-
-async function main(){
-    const users = await getUserData();
-    loadTable(users);
-}
-
-main();
+document.addEventListener("DOMContentLoaded", function () {
+    setupDigitInputs();
+});
